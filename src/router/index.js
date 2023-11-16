b860b2c0fc4699500e130cb27c456335369ca75c
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2023-11-17 00:40:28 +0900

    fix : router설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-export const router = createRouter({
+ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: () => import('@/views/MainView.vue'),
 
+    },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('@/views/LoginView.vue'),
     },
+      {
+          path: '/mypage',
+          name: 'mypage',
+          component: () => import('@/views/MypageView.vue'),
+      },
   ],
 });
 
