e839fe14d310e35b1b3c1894b6ddb35b423cc8d3
zqwz <zqwz@naver.com>
2023-11-18 02:14:54 +0900

    feat : 라우터 스케줄 상세 페이지, 작성 페이지 등록 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
- const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'main',
-      component: () => import('@/views/MainView.vue'),
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'main',
+            component: () => import('@/views/MainView.vue'),
 
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/LoginView.vue'),
-    },
-      {
-          path: '/mypage',
-          name: 'mypage',
-          component: () => import('@/views/MypageView.vue'),
-      },
-  ],
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/LoginView.vue'),
+        },
+        {
+            path: '/mypage',
+            name: 'mypage',
+            component: () => import('@/views/MypageView.vue'),
+        },
+        {
+            path: '/schedule/:id',
+            name: 'schedule',
+            component: () => import('@/components/schedule/detail/ScheduleDetailView.vue'),
+        },
+        {
+            path: '/schedule/write',
+            name: 'schedule',
+            component: () => import('@/components/schedule/write/view/ScheduleWriteView.vue'),
+        },
+
+    ],
 });
 
 export default router;
