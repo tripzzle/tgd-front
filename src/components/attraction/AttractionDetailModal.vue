fa0e3f62079cec7d6e42d47ba25510b40667a2ee
zini9188 <zqwz@naver.com>
2023-11-17 17:32:15 +0900

    feat : 일정 세부 구현

---
+++ src/components/attraction/AttractionDetailModal.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-modal
+      title="관광지 제목"
+      :open="store.open"
+      @ok="handleOk"
+      @cancel="handleCancel"
+      okText="일정 만들기"
+      cancelText="취소"
+      class="centered-title centered-content">
+  </a-modal>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import {store} from "@/stores/store";
+
+const props = defineProps(["item"]);
+</script>
+
+<style scoped>
+</style>
