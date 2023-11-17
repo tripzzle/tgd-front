e086584d34381c4e85c6a5f0bc274fd5546b27ae
wjstmdduf <wjs6265@gmail.com>
2023-11-18 05:11:09 +0900

    fix : 네브바 디자인 수정

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { store } from "@/stores/store.js";
+import {store} from "@/stores/store.js";
 import DatePicker from "@/components/common/DatePicker.vue";
 import moment from "moment";
 import "moment/locale/ko";
 import locale from "ant-design-vue/es/locale/ko_KR";
-import { ConfigProvider } from "ant-design-vue";
-import { RouterLink } from "vue-router";
+import {ConfigProvider} from "ant-design-vue";
+import {RouterLink} from "vue-router";
+
 moment.locale = "ko";
 const openModal = () => {
   console.log(store);
@@ -16,34 +17,25 @@ const openModal = () => {
 <template>
   <a-layout id="components-layout">
     <a-layout-header class="default-header">
-      <div>
+      <div class="nav-right">
         <router-link :to="{ name: 'main' }" class="app-logo">
-          <img src="@/assets/img/TGD-Logo.png" style="width: 80px" alt="" />
+          <img src="@/assets/img/TGD-Logo.png" style="width: 90px" alt=""/>
         </router-link>
       </div>
       <div class="nav-right">
-        <a-menu v-model:selectedKeys="menuKey" class="menu" mode="horizontal">
-          <a-menu-item class="custom-menu-item" key="/#">
-            <router-link :to="{ name: 'login' }">
-              <a-button class="custom-button">로그인</a-button>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item class="custom-menu-item" key="/#">
-            <router-link :to="{ name: 'mypage' }">
-              <a-button class="custom-button">로그아웃</a-button>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item class="custom-menu-item" key="/#">
-            <router-link :to="{ name: 'mypage' }">
-              <a-button class="custom-button">마이페이지</a-button>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item>
-            <a-button @click="openModal" type="primary"> 글쓰기</a-button>
-          </a-menu-item>
-        </a-menu>
+        <router-link :to="{ name: 'login' }">
+          <a-button class="custom-button">로그인</a-button>
+        </router-link>
+        <router-link :to="{ name: 'mypage' }">
+          <a-button class="custom-button">로그아웃</a-button>
+        </router-link>
+        |
+        <router-link :to="{ name: 'mypage' }">
+          <a-button class="custom-button">마이페이지</a-button>
+        </router-link>
+        <a-button class="custom-button write-button" @click="openModal" type="primary"> 글쓰기</a-button>
         <ConfigProvider :locale="locale">
-          <DatePicker />
+          <DatePicker/>
         </ConfigProvider>
       </div>
     </a-layout-header>
@@ -51,13 +43,21 @@ const openModal = () => {
 </template>
 <style lang="scss" scoped>
 .custom-menu-item {
-  width: auto !important;
+  width: auto;
+  margin: 0 !important;
 }
+
 .custom-button {
   border: none !important;
   box-shadow: none !important;
   color: #a385cf;
 }
+
+.write-button {
+  background-color: white !important;
+  border: 1px solid #a385cf !important;
+}
+
 #components-layout {
   width: 100%;
   margin-bottom: 1px;
@@ -69,6 +69,7 @@ const openModal = () => {
   background-color: white;
   box-shadow: 1px solid black;
 }
+
 .default-header {
   position: fixed;
   width: 100%;
@@ -79,22 +80,20 @@ const openModal = () => {
   justify-content: space-between;
   border: 0 0 0 1px solid black;
   box-shadow: 0 4px 4px -4px black;
+  height: 10%;
+
   .ant-menu {
     height: 98%;
   }
 }
+
 .login-button {
   font-weight: 900;
 }
+
 .nav-right {
   display: flex;
   gap: 2rem;
-  .right-buttons-wrap {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    gap: 1rem;
-    justify-content: center;
-  }
+  align-items: center;
 }
 </style>
