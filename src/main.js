39aeec866b7233b83bd9eaebc650567676ba2471
wjstmdduf <wjs6265@gmail.com>
2023-11-18 22:51:14 +0900

    fix : 로그아웃 링크 수정

--- src/main.js
+++ src/main.js
@@ -11,6 +11,15 @@ import router from './router'
 
 const app = createApp(App)
 
+// import express from 'express';
+// import cors from 'cors';
+
+// app.use(cors());
+
+// app.listen(8080, () => {
+//     console.log('서버가 8080 포트에서 실행 중입니다.');
+// })
+
 // app.use(createPinia())
 
 app.use(Antd).use(router).mount('#app');
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,69 @@
 <script setup>
-import {kakoLogin, naverLogin} from "@/assets/js/login.js";
+import {kakoLogin} from "@/assets/js/login.js";
 import {RouterLink} from "vue-router";
+import axios from "axios";
+// import { localAxios } from "@/util/http-commons";
+import {ref} from "vue";
+const {VITE_SERVER} = import.meta.env;
+
+async function localAxios() {
+  const instance = await axios.create({
+    baseURL: 'http://localhost:8080',
+    mode: 'no-cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+    credentials: 'same-origin',
+  });
+  // Request 발생 시 적용할 내용.
+  instance.defaults.headers.common["Authorization"] = "";
+  instance.defaults.headers.post["Content-Type"] = "application/json";
+  instance.defaults.headers.put["Content-Type"] = "application/json";
+
+  return instance;
+}
+
+const local = localAxios();
+
+async function login(success, fail) {
+   (await local).get(`oauth2/authorization/naver`).then(success).catch(fail);
+}
+
+const NaverLogin = async () => {
+  await login(
+      (response) => {
+        if (response.status === httpStatusCode.CREATE) {
+          let { data } = response;
+          let accessToken = data["access-token"];
+          let refreshToken = data["refresh-token"];
+          isLogin.value = true;
+          isLoginError.value = false;
+          isValidToken.value = true;
+          sessionStorage.setItem("accessToken", accessToken);
+          sessionStorage.setItem("refreshToken", refreshToken);
+        } else {
+          isLogin.value = false;
+          isLoginError.value = true;
+          isValidToken.value = false;
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+  );
+};
+async function KakaoLogin() {
+  try {
+    const response = await axios.get(
+        `${VITE_SERVER}/oauth2/authorization/kakao`
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <template>
@@ -11,17 +74,24 @@ import {RouterLink} from "vue-router";
 
     <a-row class="login-container" justify="center" align="middle">
       <a-col align="center" :span="24">
-        <img
-            class="login-button-img"
-            src="@/assets/img/login/kakao_login_medium_wide.png"
-            @click="kakoLogin.kakoLogin"
-        />
+        <a href="http://localhost:8080/oauth2/authorization/kakao">
+          <img
+              class="login-button-img"
+              src="@/assets/img/login/kakao_login_medium_wide.png"
+              @click="kakoLogin.kakoLogin"
+          />
+        </a>
       </a-col>
       <a-col align="center" :span="24">
-        <img src="@/assets/img/login/btnG_완성형.png" style="width: 200px; height: 45px;" @click="naverLogin">
+<!--        <a href="http://localhost:8080/oauth2/authorization/naver">-->
+<!--         -->
+<!--        </a>-->
+        <img src="@/assets/img/login/btnG_완성형.png" style="width: 200px; height: 45px;" @click="NaverLogin">
       </a-col>
       <a-col align="center" :span="24">
-        <img style="width: 190px; height: 40px" src="@/assets/img/login/web_light_sq_SU@3x.png"/>
+        <a href="http://localhost:8080/oauth2/authorization/google">
+          <img style="width: 190px; height: 40px" src="@/assets/img/login/web_light_sq_SU@3x.png"/>
+        </a>
       </a-col>
     </a-row>
   </div>
