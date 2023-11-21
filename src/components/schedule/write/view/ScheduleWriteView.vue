a2ca4076b0e60b68cbb5b0a1b67b1382d2459f3e
zqwz <zqwz@naver.com>
2023-11-21 23:04:46 +0900

    feat : 일정 post 화면 구성

--- src/components/schedule/write/view/ScheduleWriteView.vue
+++ src/components/schedule/write/view/ScheduleWriteView.vue
@@ -7,7 +7,67 @@
                        @removeAttraction="handleRemoveAttraction" @delete-to-list="handleRemoveAttraction"/>
     <MapView :attraction="goToAttraction"/>
   </a-flex>
-  <a-button style="width: 100%" @click="postSchedule">일정 만들기</a-button>
+  <a-button type="default" @click="showDrawer" style="width: 100%">
+    일정 만들기
+  </a-button>
+  <a-drawer
+      title="추가 일정 정보를 입력하세요"
+      :width="720"
+      :open="open"
+      :body-style="{ paddingBottom: '80px' }"
+      :footer-style="{ textAlign: 'right' }"
+      @close="onClose"
+  >
+    <a-form :model="form" :rules="rules" layout="vertical" ref="formRef">
+      <a-row :gutter="16">
+        <a-col :span="24">
+          <a-form-item label="제목" name="title">
+            <a-input v-model:value="form.title" placeholder="제목을 입력하세요"/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="16">
+        <a-col :span="24">
+          <a-form-item label="소개" name="content">
+            <a-textarea v-model:value="form.content" :rows="4" placeholder="소개를 작성해주세요"/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="16">
+        <a-col :span="24">
+          <a-form-item label="일정 이미지" name="image">
+            <a-upload-dragger
+                v-model:fileList="fileList"
+                name="file"
+                :max-count="1"
+                @change="handleChange"
+                @drop="handleDrop"
+            >
+              <p class="ant-upload-drag-icon">
+                <inbox-outlined></inbox-outlined>
+              </p>
+              <p class="ant-upload-text">영역에 이미지를 드래그하거나 영역을 클릭하세요</p>
+              <p class="ant-upload-hint">
+                일정 이미지로 사용하고 싶은 배경 사진을 선택하세요
+              </p>
+            </a-upload-dragger>
+          </a-form-item>
+          <a-form-item label="공개여부" name="viewYn">
+            <a-radio-group v-model:value="form.viewYn">
+              <a-radio value="false">공개</a-radio>
+              <a-radio value="true">비공개</a-radio>
+            </a-radio-group>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+    <template #extra>
+      <a-space>
+        <a-button @click="onClose">취소</a-button>
+        <a-button type="primary" @click="post">생성</a-button>
+      </a-space>
+    </template>
+  </a-drawer>
 </template>
 
 <script setup>
@@ -25,10 +85,7 @@ let days = ref();
 let showAttractionView = ref(false);
 const currentSlide = ref(0);
 const goToAttraction = ref();
-
-const postSchedule = () => {
-  console.log("일정 생성 버튼 클릭", days.value);
-}
+const server = import.meta.env.VITE_SERVER;
 
 // 관광지를 삭제하는 메서드, 현재 일자에 해당하는 동일한 관광지를 지워준다.
 const handleRemoveAttraction = (attraction, currentSlide, index) => {
@@ -79,10 +136,123 @@ const updateDatesFromQuery = () => {
 };
 
 onMounted(updateDatesFromQuery);
+
+const form = reactive({
+  title: '',
+  content: '',
+  viewYn: '',
+  userId: 1
+});
+const rules = {
+  title: [
+    {
+      required: true,
+      message: '제목을 입력해주세요',
+    },
+  ],
+  content: [
+    {
+      required: true,
+      message: '일정 상세 내용을 입력해주세요',
+    },
+  ],
+  viewYn: [
+    {
+      required: true,
+      message: '공개여부를 선택해주세요',
+    },
+  ],
+};
+const open = ref(false);
+import {message} from 'ant-design-vue';
+import axios from "axios";
+import router from "@/router";
+
+// 파일 저장 객체
+const fileList = ref(null);
+// 업로드할 파일을 선택하는 경우 발생하는 이벤트
+const handleChange = info => {
+  console.log("infofile", info.file)
+  fileList.value = info.file;
+};
+
+function handleDrop(e) {
+  console.log(e);
+  fileList.value = e.target.value;
+}
+
+const showDrawer = () => {
+  open.value = true;
+};
+const onClose = () => {
+  open.value = false;
+};
+
+// form 유효성 검사를 위한 객체
+const formRef = ref(null);
+const post = async () => {
+  try {
+    // form의 유효성 검사
+    await formRef.value.validate();
+
+    let formData = new FormData();
+    let json = JSON.stringify(form);
+    console.log("json", json);
+
+    // 프로필 이미지 formData에 추가
+    formData.append('image', fileList.value);
+
+    // 일자에 있는 관광지, 메모들 form에 붙이기
+    form.days = days.value.map(day => ({
+      ...day,
+      dayAttractions: day.attractions,
+    }));
+    console.log(days.value)
+    // formData application/json 타입으로 붙이기
+    formData.append('post', new Blob([JSON.stringify(form)], {type: 'application/json'}));
+    console.log(formData);
+    // 서버로 post요청
+    await axios.post(`${server}/api/schedule`,
+        formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+    )
+        .then(response => {
+          const parts = response.headers.get('location').split('/');
+          const id = parts[parts.length - 1];
+          console.log(id);
+          router.push({name: 'scheduleDetail', params: {id: id}});
+        })
+        .catch(error => {
+          // 요청이 실패했을 때의 동작
+          console.error(error);
+        });
+  } catch (errors) {
+    console.log(errors);
+    message.error('모든 필드를 올바르게 입력해 주세요.');
+  }
+}
 </script>
 
 <style scoped>
 .space {
   margin: 60px 20px;
+}
+
+.avatar-uploader > .ant-upload {
+  width: 128px;
+  height: 128px;
+}
+
+.ant-upload-select-picture-card i {
+  font-size: 32px;
+  color: #999;
+}
+
+.ant-upload-select-picture-card .ant-upload-text {
+  margin-top: 8px;
+  color: #666;
 }
 </style>
