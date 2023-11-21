374951ba2d9716278e602d0faa904ce98b93c274
wjstmdduf <wjs6265@gmail.com>
2023-11-21 17:57:31 +0900

    refactor : 서버에서 유저정보 받아옴

--- src/components/Mypage/UserInfo.vue
+++ src/components/Mypage/UserInfo.vue
@@ -1,17 +1,46 @@
-<script setup></script>
+<script setup>
+import {defineProps, onMounted} from "vue";
+
+const userInfo = defineProps({
+  userName: {
+    type: String
+  },
+  userNickName: {
+    type: String
+  },
+  userImg: {
+    type: String
+  }
+  , userSex: {
+    type: String
+  }
+});
+
+console.log("test");
+
+function sex() {
+  return userInfo.userSex ? "남성" : "여성";
+}
+
+console.log("유저 성별" + sex)
+onMounted(() => {
+  console.log("유저정보 잘옴" + userInfo.userNickName)
+})
+</script>
 
 <template>
   <div>
     <a-row>
       <a-avatar class="profil" :size="144">
         <template #icon>
-          <img src="@/assets/img/pxfuel.jpg" />
+          <img :src="userImg"/>
         </template>
       </a-avatar>
       <a-col class="profile-details">
         <a-col>
-          <h3>닉네임 : 난밤이야 슈밤</h3>
-          <h3>이름 : 전승열</h3>
+          <h3>닉네임 : {{ userInfo.userNickName }}</h3>
+          <h3>이름 : {{ userInfo.userName }}</h3>
+          <h3>성별 : {{ sex() }}</h3>
         </a-col>
         <a-col>
           <a-button class="profil-btn" size="small">수정</a-button>
@@ -26,10 +55,12 @@
 h3 {
   margin-top: 20px;
 }
+
 .profil {
   margin-right: 80px;
   border: 2px solid black; /* Add border */
 }
+
 .profile-details {
   display: flex;
   flex-direction: column;
