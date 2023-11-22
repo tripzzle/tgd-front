8cd1970e682f49ceec877481619338f7f3823869
wjstmdduf <wjs6265@gmail.com>
2023-11-23 02:24:38 +0900

    refactor : 유저 일정정보, 유저 위시리스트 추가

--- src/components/Mypage/components/MySchedule/ScheduleList.vue
+++ src/components/Mypage/components/MySchedule/ScheduleList.vue
@@ -1,14 +1,62 @@
 <script setup>
 import ScheduleDetail from "@/components/Mypage/components/MySchedule/ScheduleDetail.vue";
+
+import {onMounted, ref} from "vue";
+import axios from "axios";
+import {user} from "@/stores/store";
+import InfiniteLoading from "v3-infinite-loading";
+const server = import.meta.env.VITE_SERVER;
+
+const userSchedules = ref({});
+
+onMounted(async () => {
+
+  await axios.get(`${server}/api/user/userSchedule`,{
+    headers: {
+      'X-AUTH-TOKEN': user.token
+    }
+  })
+      .then(function (response){
+        userSchedules.value = response.data;
+      })
+      .catch(function (error){
+        console.log(error)
+      })
+})
 </script>
 
 <template>
-  <a-row justify="center">
-    <ScheduleDetail/>
-    <ScheduleDetail/>
-    <ScheduleDetail/>
-  </a-row>
+  <a-col  class="result" :gutter="[30, 80]">
+    <ScheduleDetail v-for="schedule in userSchedules" :key="schedule.scheduleId" :schedule="schedule"/>
+  </a-col>
 </template>
 
 <style scoped>
+.result {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-weight: 300;
+  width: 400px;
+  padding: 10px;
+  text-align: center;
+  justify-items: center;
+  margin: 0 auto 10px auto;
+  border-radius: 10px;
+  overflow-y: scroll;
+  max-height: 550px;
+}
+
+.result::-webkit-scrollbar {
+  width: 0.8em; /* 스크롤바 너비 */
+  background-color: transparent; /* 스크롤바 배경색 */
+}
+
+.result::-webkit-scrollbar-thumb {
+  background-color: transparent; /* 스크롤바 색상 */
+}
+
+.result::-webkit-scrollbar-track {
+  background-color: transparent; /* 스크롤바 트랙 배경색 */
+}
 </style>
--- src/components/Mypage/components/WishAttraction/AttractionWishList.vue
+++ src/components/Mypage/components/WishAttraction/AttractionWishList.vue
@@ -1,32 +1,79 @@
 <script setup>
-import {ref} from "vue"
+import { onMounted, ref } from "vue";
 import AttractionDetail from "@/components/attraction/AttractionDetail.vue";
+import axios from "axios";
+import { user } from "@/stores/store";
 
-const wishListA = ref([
-  {
-    imgUrl: "",
-    title:"test1",
-    address:"test1test1test1test1"
-  },
-  {
-    imgUrl: "",
-    title:"test2",
-    address:"test2test2test2test2test2"
-  },
-  {
-    imgUrl: "",
-    title:"test3",
-    address:"test3test3test3test3"
-  },
-]);
+const server = import.meta.env.VITE_SERVER;
 
+const wishListA = ref({});
+
+onMounted(async () => {
+  await axios
+      .get(`${server}/api/user/userWishAT`, {
+        headers: {
+          'X-AUTH-TOKEN': user.token
+        },
+      })
+      .then(function (response) {
+        console.log(response.data);
+        wishListA.value = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+});
 </script>
 
 <template>
-  <a-row justify="center" :gutter="[0, 32]">
-    <AttractionDetail v-for="item in wishListA" :key="item.attractionId" :item="item" @click="showDetail(item)" class="list" />
-  </a-row>
+  <a-col class="result">
+    <div class="schedule-card-container">
+      <AttractionDetail
+          v-for="item in wishListA"
+          :key="item.attractionId"
+          :item="item"
+          @click="showDetail(item)"
+          class="list"
+      />
+    </div>
+  </a-col>
 </template>
 
 <style scoped>
+.result {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-weight: 300;
+  width: 400px;
+  padding: 10px;
+  text-align: center;
+  justify-items: center;
+  margin: 0 auto 10px auto;
+  border-radius: 10px;
+  overflow-y: auto; /* 상하 스크롤 생성 */
+  max-height: 550px;
+}
+
+.schedule-card-container {
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 5px;
+  /* overflow-x: scroll; */ /* 좌우 스크롤 생성 */
+  scroll-snap-type: x mandatory; /* 좌우 스크롤 스냅 설정 */
+  scroll-padding: 0 10px; /* 좌우 스크롤 패딩 설정 */
+}
+
+.result::-webkit-scrollbar {
+  width: 0.8em; /* 스크롤바 너비 */
+  background-color: transparent; /* 스크롤바 배경색 */
+}
+
+.result::-webkit-scrollbar-thumb {
+  background-color: transparent; /* 스크롤바 색상 */
+}
+
+.result::-webkit-scrollbar-track {
+  background-color: transparent; /* 스크롤바 트랙 배경색 */
+}
 </style>
